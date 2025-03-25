9335be12398bdd36c6b677a6d273ce3355430c47
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2025-03-25 13:54:21 +0300

    Calendar status

--- src/models/initDb.js
+++ src/models/initDb.js
@@ -80,6 +80,7 @@ const createCalendarEventsTable = () => {
     CREATE TABLE IF NOT EXISTS calendar_events (
       id INT AUTO_INCREMENT PRIMARY KEY,
       title VARCHAR(255) NOT NULL,
+      status ENUM('done', 'planned') DEFAULT 'planned',
       start_time TIME,
       event_date DATE NOT NULL,
       duration VARCHAR(10),
